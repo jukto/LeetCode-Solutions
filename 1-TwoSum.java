126164a3e06dbf62428740cf5fd5d608932d171c
jukto <36252256+jukto@users.noreply.github.com>
2023-07-07 19:33:27 -0500

    Update 1-TwoSum.java

--- 1-TwoSum.java
+++ 1-TwoSum.java
@@ -3,12 +3,12 @@ class Solution {
         Map<Integer, Integer> valueToIndex = new HashMap<Integer, Integer>();
         for(int i = 0; i < nums.length; i++) {
             int need = target - nums[i];
-            Integer vv = valueToIndex.get(need);
-            if (vv != null) {
-                return new int[] {i, valueToIndex.get(need)};
+            Integer i2 = valueToIndex.get(need);
+            if (i2 != null) {
+                return new int[] {i, i2};
             }
             valueToIndex.put(nums[i], i);
         }
-        return nums;
+        return null;
     }
 }
