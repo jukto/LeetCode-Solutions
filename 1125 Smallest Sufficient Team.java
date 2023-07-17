af54b839b776effb62bbd15495e38f1f54241374
jukto <36252256+jukto@users.noreply.github.com>
2023-07-17 13:22:44 -0500

    Update 1125 Smallest Sufficient Team.java

--- 1125 Smallest Sufficient Team.java
+++ 1125 Smallest Sufficient Team.java
@@ -1,106 +1,78 @@
 class Solution {
-    private static Map<String, Integer> bitMap = new HashMap<String, Integer>();
+    private static int[] runningResult;
 
     public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> people) {
         List<Person> usefulPeople = new ArrayList<Person>(people.size());
-        LinkedList<Team> queue = new LinkedList<Team>();
-        
-        int bit = 1;
-        int bitGoal = 0;
+        PriorityQueue<Person> peopleHeap = new PriorityQueue<Person>( (a, b) -> b.skillCount - a.skillCount );
+        Map<String, Integer> skillToBitMap = new HashMap<String, Integer>(req_skills.length);
+        int bit = 1, skillGoal = 0;
+        runningResult = null;
 
         for (String skill : req_skills) {
-            bitMap.put(skill, bit);
-            bitGoal |= bit;
+            skillToBitMap.put(skill, bit);
+            skillGoal |= bit;
             bit <<= 1;
         }
 
         for (int i = 0; i < people.size(); i++) {
-            usefulPeople.add(new Person(i, people.get(i)));
+            List<String> skillList = people.get(i);
+            int skills = 0;
+
+            for (String skill : skillList) {
+                skills |= skillToBitMap.get(skill);
+            }
+            
+            peopleHeap.add(new Person(i, skills, skillList.size()));
         }
 
-        usefulPeople.sort(new myComparator());
-        bit = 1;
-
-        for (int i = 0; i < usefulPeople.size(); i++) {
-            var person1 = usefulPeople.get(i);
-            person1.peopleBit = bit;
-            person1.usefulIndex = i;
-            queue.add(new Team(person1));
-            for (int k = usefulPeople.size() - 1; k > i; k--) {
-                var person2 = usefulPeople.get(k);
-                if ((person1.skills & person2.skills) == person2.skills) {
-                    usefulPeople.remove(k);
+        outerloop:
+        do {
+            Person person1 = peopleHeap.poll();
+            for (Person person2 : usefulPeople) {
+                if ((person1.skills & person2.skills) == person1.skills) {
+                    continue outerloop;
                 }
             }
-            bit <<= 1;
+            usefulPeople.add(person1);
+        } while (!peopleHeap.isEmpty());
+
+        recursion(usefulPeople, skillGoal, new ArrayList<Person>(usefulPeople.size()), 0, -1);
+        return runningResult;
+    }
+
+
+    private void recursion(List<Person> usefulPeople, int skillGoal, List<Person> runningTeam, int runningSkills, int lastIndex) {
+        if (runningResult != null && runningResult.length <= runningTeam.size()) {
+            return;
         }
 
-        while (true) {
-            var team = queue.pop();
-            if (team.skills == bitGoal) {
-                int j = 0, k = 0;
-                
-                int[] result = new int[team.size];
-
-                while (team.peopleBits > 0) {
-                    if ((team.peopleBits & 1) == 1) {
-                        result[j] = usefulPeople.get(k).realIndex;
-                        j++;
-                    }
-                    k++;
-                    team.peopleBits >>= 1;
+        if (runningSkills == skillGoal) {
+            if (runningResult == null || runningResult.length > runningTeam.size()) {
+                runningResult = new int[runningTeam.size()];
+                for (int i = 0; i < runningTeam.size(); i++) {
+                    runningResult[i] = runningTeam.get(i).realIndex;
                 }
-
-                return result;
             }
-
-            team.size++;
-
-            for (int k = team.lastIndex + 1; k < usefulPeople.size(); k++) {
-                var p = usefulPeople.get(k);
-                if ((team.skills & p.skills) != p.skills) {
-                    queue.add(new Team(team, p));
-                }
+            return;
+        }
+        
+        for (int i = lastIndex + 1; i < usefulPeople.size(); i++) {
+            Person p = usefulPeople.get(i);
+            if ((runningSkills & p.skills) != p.skills) {
+                runningTeam.add(p);
+                recursion(usefulPeople, skillGoal, runningTeam, runningSkills | p.skills, i);
+                runningTeam.remove(runningTeam.size() - 1);
             }
         }
     }
 
     private class Person {
-        public int skills, skillCount, realIndex, usefulIndex;
-        public long peopleBit;
+        public int skills, skillCount, realIndex;
 
-        public Person(int i, List<String> skillList) {
-            realIndex = i;
-            skillCount = skillList.size();
-            for (String skill : skillList) {
-                skills |= bitMap.get(skill);
-            }
-        }
-    }
-
-    private class Team {
-        public int skills, size, lastIndex;
-        public long peopleBits;
-
-        public Team(Person p) {
-            skills = p.skills;
-            lastIndex = p.usefulIndex;
-            peopleBits = p.peopleBit;
-            size = 1;
-        }
-
-        public Team(Team team, Person p) {
-            skills = team.skills | p.skills;
-            lastIndex = p.usefulIndex;
-            peopleBits = team.peopleBits | p.peopleBit;
-            size = team.size;
-        }
-    }
-        
-    private class myComparator implements Comparator<Person> {
-        @Override
-        public int compare(Person a, Person b) {
-            return b.skillCount - a.skillCount;
+        public Person(int realIndex, int skills, int skillCount) {
+            this.realIndex = realIndex;
+            this.skills = skills;
+            this.skillCount = skillCount;
         }
     }
 }
