cfca63089936e5802fa8fda0a3ef448697fc7021
jukto <36252256+jukto@users.noreply.github.com>
2023-07-22 17:49:39 -0500

    Update 738 Monotone Increasing Digits.java

--- 738 Monotone Increasing Digits.java
+++ 738 Monotone Increasing Digits.java
@@ -2,12 +2,9 @@ class Solution {
     private static final int[] powersOfTen = new int[] { 1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000 };
 
     public int monotoneIncreasingDigits(int n) {
-        if (n < 10) return n;
-
         for (int i = 0; i < 9; i++) {
-            if(getDigit(n, i + 1) > getDigit(n, i)) {
+            while (getDigit(n, i + 1) > getDigit(n, i)) {
                 n -= (n % powersOfTen[i]) + 1;
-                i--;
             }
         }
 
