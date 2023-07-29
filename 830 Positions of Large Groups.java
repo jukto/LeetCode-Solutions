a88059e8481379382ad1ed9a367ddfea7dbb81eb
jukto <36252256+jukto@users.noreply.github.com>
2023-07-29 13:05:20 -0500

    Create 830 Positions of Large Groups.java

---
+++ 830 Positions of Large Groups.java
@@ -0,0 +1,24 @@
+//  Problem: https://leetcode.com/problems/positions-of-large-groups/submissions/
+
+class Solution {
+    public List<List<Integer>> largeGroupPositions(String s) {
+        ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
+        int start = 0, i = 0;
+        char last = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c != last) {
+                if (i - start >= 3)
+                    result.add(List.of(start, i - 1));
+                last = c;
+                start = i;
+            }
+            i++;
+        }
+
+        if (i - start >= 3)
+            result.add(List.of(start, i - 1));
+
+        return result;
+    }
+}
