21ac510dc6792561cacc3a65f5a1568891b03cc6
jukto <36252256+jukto@users.noreply.github.com>
2023-08-01 10:17:55 -0500

    Update 77 Combinations.java

--- 77 Combinations.java
+++ 77 Combinations.java
@@ -2,11 +2,11 @@
 
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        return recursion(new ArrayList<List<Integer>>(), n, k, 0, new int[20], 0);
+        return recursion(new ArrayList<List<Integer>>(), n, k, 0, new Integer[k], 0);
     }
 
-    private List<List<Integer>> recursion(List<List<Integer>> result, int n, int k, int last, int[] combo, int comboLength) {
-        if (comboLength == k) result.add( makeList(combo, comboLength) );
+    private List<List<Integer>> recursion(List<List<Integer>> result, int n, int k, int last, Integer[] combo, int comboLength) {
+        if (comboLength == k) result.add( Arrays.asList(Arrays.copyOf(combo, comboLength)) );
         else {
             // The next value we add must be greater than the last added value, and leave at least (k - length of combination) values after it.
             for (int i = n - k + comboLength + 1; i > last; i--) {
@@ -15,14 +15,5 @@ class Solution {
             }
         }
         return result;
-    }
-
-    // Since k has a maximum value of 20, we can use an array and a pointer to keep track of the current combination instead of a list.
-    // This helper function converts that into the List<Integer> that we are expected to return.
-    private List<Integer> makeList(int[] array, int arrayLength) {
-        List<Integer> list = new ArrayList<Integer>(arrayLength);
-        for (int i = 0; i < arrayLength; i++)
-            list.add(array[i]);
-        return list;
     }
 }
