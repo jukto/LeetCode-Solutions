929bb5e7548377c8f14b5a2666ddcde019840b1c
Josh Garnto <36252256+jukto@users.noreply.github.com>
2024-04-25 15:02:55 -0500

    Create 586 Customer Placing the Largest Number of Orders.sql

---
+++ 586 Customer Placing the Largest Number of Orders.sql
@@ -0,0 +1,7 @@
+# Problem: https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/
+
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(customer_number) DESC
+LIMIT 1
