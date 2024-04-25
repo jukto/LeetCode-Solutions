27fb137efa3e4902aa9218f5610b99be6c3c02f6
Josh Garnto <36252256+jukto@users.noreply.github.com>
2024-04-25 14:48:07 -0500

    Create 197 Rising Temperature.sql

---
+++ 197 Rising Temperature.sql
@@ -0,0 +1,7 @@
+# Problem: https://leetcode.com/problems/rising-temperature/
+
+SELECT w2.id
+FROM Weather w1, Weather w2
+WHERE
+    DATEDIFF(w2.recordDate, w1.recordDate) = 1
+    AND w2.temperature > w1.temperature
