fe9fcac30e6f7e590bb34a5d241bedcc2ea62352
Josh Garnto <36252256+jukto@users.noreply.github.com>
2024-04-26 10:05:05 -0500

    Create 619 Biggest Single Number.sql

---
+++ 619 Biggest Single Number.sql
@@ -0,0 +1,9 @@
+# Problem: https://leetcode.com/problems/biggest-single-number/
+
+SELECT MAX(num) as num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+) sub
