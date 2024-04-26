f4d592773298a648dcb80f7cc2d2548357e65276
Josh Garnto <36252256+jukto@users.noreply.github.com>
2024-04-26 10:15:11 -0500

    Create 1050 Actors and Directors Who Cooperated At Least Three Times.sql

---
+++ 1050 Actors and Directors Who Cooperated At Least Three Times.sql
@@ -0,0 +1,6 @@
+# Problem: https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
+
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(actor_id) >= 3
