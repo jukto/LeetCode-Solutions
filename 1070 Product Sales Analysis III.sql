d4bb7e3e90573c6a2ec909342b5bb1ddbfcfcf07
Josh Garnto <36252256+jukto@users.noreply.github.com>
2024-04-27 11:14:13 -0500

    Create 1070 Product Sales Analysis III.sql

---
+++ 1070 Product Sales Analysis III.sql
@@ -0,0 +1,9 @@
+# Problem: https://leetcode.com/problems/product-sales-analysis-iii/
+
+SELECT product_id, year first_year, quantity, price
+FROM Sales
+WHERE (product_id, year) IN (
+    SELECT product_id, MIN(year)
+    FROM Sales
+    GROUP BY product_id
+)
