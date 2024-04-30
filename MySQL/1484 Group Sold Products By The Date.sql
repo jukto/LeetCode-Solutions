ae494b194c1f00b5bb3251623482144ce1d2e584
Josh Garnto <36252256+jukto@users.noreply.github.com>
2024-04-30 09:19:54 -0500

    Create 1484 Group Sold Products By The Date.sql

---
+++ MySQL/1484 Group Sold Products By The Date.sql
@@ -0,0 +1,9 @@
+# Problem: https://leetcode.com/problems/group-sold-products-by-the-date/
+
+SELECT
+    sell_date,
+    COUNT(DISTINCT product) num_sold,
+    GROUP_CONCAT(DISTINCT product ORDER BY product) products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date
