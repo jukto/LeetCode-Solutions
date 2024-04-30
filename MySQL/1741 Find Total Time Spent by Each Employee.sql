f6a0a20fd30c5517bf6b6228d6abdded0fc80122
Josh Garnto <36252256+jukto@users.noreply.github.com>
2024-04-30 09:57:14 -0500

    Create 1741 Find Total Time Spent by Each Employee.sql

---
+++ MySQL/1741 Find Total Time Spent by Each Employee.sql
@@ -0,0 +1,8 @@
+# Problem: https://leetcode.com/problems/find-total-time-spent-by-each-employee/
+
+SELECT
+    event_day day,
+    emp_id,
+    SUM(out_time - in_time) total_time
+FROM Employees
+GROUP BY event_day, emp_id
