6d2c05b033021f82ba794a5de82a39ae29f2c880
Josh Garnto <36252256+jukto@users.noreply.github.com>
2024-06-27 10:00:50 -0500

    Create 1791. Find Center of Star Graph.cs

---
+++ C#/1791. Find Center of Star Graph.cs
@@ -0,0 +1,7 @@
+// Problem: https://leetcode.com/problems/find-center-of-star-graph/
+
+public class Solution {
+    public int FindCenter(int[][] edges) {
+        return (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) ? edges[0][0] : edges[0][1];
+    }
+}
